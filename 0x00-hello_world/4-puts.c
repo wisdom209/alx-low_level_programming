b654efb057b37d5e262fdfb6037b942fd1073195
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-08 12:16:13 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point for the program
+ */ 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
