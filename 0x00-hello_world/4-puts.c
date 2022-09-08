cb2a54865ad56a6215199b9c50247823a4b3a9c0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-08 12:25:16 -0700

    fix betty lint

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,11 @@
 
 /**
  * main - Entry point for the program
- */ 
+ * @void: Takes no arguments
+ *
+ * Description: This function just prints out text.
+ * Return: 0 if successful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
