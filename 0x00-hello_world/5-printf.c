97ce42e6cf042a8b590d785ed9cf11f765ef92d7
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-08 12:31:57 -0700

    Write a C program that prints exactly using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of programme
+* @void: This function takes no arguments
+*
+* Description: Prints out text to standard output
+* Return: 0 if successful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
+
+
