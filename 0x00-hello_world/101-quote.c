7cac352ae8d577be133d7a88e68f0c3842c9b9d8
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-08 17:23:21 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 * Return: 0 if successful
 */
 int main(void)
-{	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, buf, sizeof(buf));
-	write(1, "\n", 1);
 	return (1);
 }
