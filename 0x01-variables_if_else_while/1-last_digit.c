e562f0dce881f834be6b69e4692df385fe0e22f5
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-09 09:13:03 -0700

    flow control

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* @void: no args
+*
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int lastDigit = n % 10;
+
+	if (lastDigit < 0)
+		lastDigit = lastDigit * -1;
+
+	if (lastDigit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+
+	if (lastDigit == 0)
+		printf("Last digit is %d is 0 and is 0\n", n);
+
+	if (lastDigit < 6 && lastDigit != 0)
+		printf("Last digit of of %d is %d and not 0\n", n, lastDigit);
+	return (0);
+}
