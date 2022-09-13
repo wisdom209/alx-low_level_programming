8fdb061372f9fa0193e9d25fa07081bc83db6cbe
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-13 16:35:52 -0700

    fibo till 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @void: takes no args
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	double i;
+	double end;
+	double t1;
+	double t2;
+	double nextTerm;
+
+	end = 98;
+	t1 = 1;
+	t2 = 2;
+
+	printf("%.0f, %.0f, ", t1, t2);
+
+	for (i = 3; i < end; i++)
+	{
+
+		nextTerm = t1 + t2;
+		if (i == end - 1)
+		{
+			printf("%.0f\n", nextTerm);
+		}
+		else
+		{
+			printf("%.0f, ", nextTerm);
+		}
+
+		t1 = t2;
+		t2 = nextTerm;
+	}
+}
