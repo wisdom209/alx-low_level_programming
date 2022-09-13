7dcf573daabad5f581d6611c84cb2799a7ffdf10
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-13 15:57:09 -0700

    hot fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,16 @@ int main(void)
 	long t2;
 	long nextTerm;
 	int i;
+	int end;
 
 	t1 = 0;
 	t2 = 1;
+	end = 50;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < end; i++)
 	{
 		nextTerm = t1 + t2;
-		if (i == 48)
+		if (i == end)
 		{
 			printf("%lu", nextTerm);
 		}
