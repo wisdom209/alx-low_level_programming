ec55329cfe43abd17c092d36ba9f96f391cdf5c9
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-13 16:14:04 -0700

    more fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @void: takes no args
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long double t1;
+	long double t2;
+	long long int nextTerm;
+	long double max;
+	long double sum;
+
+	t1 = 0;
+	t2 = 1;
+	nextTerm = 1;
+	max = 4000000;
+	sum = 0;
+
+	while (nextTerm < max)
+	{
+		if ((nextTerm % 2) == 0)
+		{
+			sum = nextTerm + sum;
+		}
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+
+	printf("%.0Lf\n", sum + 2);
+
+	return (0);
+}
