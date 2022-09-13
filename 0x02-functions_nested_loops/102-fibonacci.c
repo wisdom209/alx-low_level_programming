6db066f6e4dd077a1799a6c2e021e9fb1d04865f
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-13 15:49:19 -0700

    first 50 fibo nums

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @void: takes no args
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long t1;
+	long t2;
+	long nextTerm;
+	int i;
+
+	t1 = 0;
+	t2 = 1;
+
+	printf("%lu, ", t2);
+
+	for (i = 0; i < 49; i++)
+	{
+		nextTerm = t1 + t2;
+		if (i == 48)
+		{
+			printf("%lu", nextTerm);
+		}
+		else
+		{
+			printf("%lu, ", nextTerm);
+		}
+
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("\n");
+}
