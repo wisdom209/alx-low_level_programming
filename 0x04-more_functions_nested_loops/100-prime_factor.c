43edccad51a8942e2424cab431fba27cfeb3e140
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-15 16:41:40 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @void: takes no function
+ *
+ * Return: returns nothing
+ */
+int main(void)
+{
+
+	unsigned int max, i, j, k, maxPrime;
+
+	max = 100;
+	maxPrime = 0;
+
+	for (i = 2; i <= max; i++)
+	{
+		for (j = 2; j <= max; j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (i == j && max % i == 0)
+		{
+			if (i > maxPrime)
+			{
+				maxPrime = i;
+			}
+		}
+	}
+	printf("%d", maxPrime);
+	printf("\n");
+}
