51e3017157d9205a4233a35a027e18d149f8f8c1
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-15 09:16:21 -0700

    check if char is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if character is uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
