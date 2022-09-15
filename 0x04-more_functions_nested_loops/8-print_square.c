1d7d77c465f6e53dae01b186163382f9c869bb36
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-15 11:30:18 -0700

    print square with hashes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square with hashes
+ * @size: number of lines of square
+ *
+ * Return - returns nothing
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
