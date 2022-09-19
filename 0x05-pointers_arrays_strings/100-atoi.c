693c5704b9a784405f6f925b24fcb12b5ae03fcc
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-19 15:39:25 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,7 @@ int _atoi(char *s)
 
 	c = 0;
 	sum = 0;
+	j = 0;
 
 	while (s[c] != '\0')
 	{
@@ -21,8 +22,7 @@ int _atoi(char *s)
 		if (s[i] >= 48 && s[i] <= 57)
 		{
 			sum = sum * 10 + (s[i] - 48);
-			if (i == c - 1)
-				break;
+
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
@@ -30,8 +30,6 @@ int _atoi(char *s)
 
 	while (s[j] <= 48 || s[j] >= 57)
 	{
-		if (j == c - 1)
-			break;
 		if (s[j] == '-')
 			numNeg++;
 		if (s[j] == '+')
