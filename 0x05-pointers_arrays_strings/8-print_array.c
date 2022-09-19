5d997c232ab79bf6af25b93cf98c543bd43e03b0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-19 13:34:23 -0700

    print an array of numbers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - traverse and print an array
+ * @a: array to print
+ * @n: length of array to print
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
