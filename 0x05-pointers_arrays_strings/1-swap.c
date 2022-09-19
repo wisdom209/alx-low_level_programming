07546ffd7eed82309cabe2544553e0a4eed9bb60
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-19 11:28:04 -0700

    swap two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swap two int variables
+ * @a: integer variable
+ * @b: integer variable
+ *
+ * Return: nothing
+ *//*  */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;/*  */
+	*b = temp;
+}
