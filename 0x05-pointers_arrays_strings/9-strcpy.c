9912e6a229ce7866e022a4dddab4ccdf39ccf536
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-19 14:21:39 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+/**
+ * _strcopy - copies a string
+ * @dest: where to copy to
+ * @src: where to copy from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c, i;
+
+	c = 0;
+
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+
+	for (i = 0; i < c; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
