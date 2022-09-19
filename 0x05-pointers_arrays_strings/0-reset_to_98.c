21eb9fce4c7c6c3e923ccf6dc6613a1906202961
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-19 11:24:03 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - update value of pointer to 98
+ * @n: pointer variable
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Projects on Arrays, Pointers and Strings
