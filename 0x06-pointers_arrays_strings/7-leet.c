2f8fb59197b4aab50d91656530c7756cce602cc2
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-22 10:51:19 -0700

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,33 @@
 #include "main.h"
 /**
- * leet - 1377 encryption of strings
- * @s: string to encode
- *
- * Return: encoded string s
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
  */
 char *leet(char *s)
 {
-	int i, j, k = 0;
-	char ref[] = "aeotlAEOTL";
-	char dest[] = "4307143071";
+	int i;
+	int j;
+	int k;
 
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
 		k = 0;
-
-		while (ref[j] != '\0')
+		while (l[j] != '\0')
 		{
-			if (s[i] == ref[j])
+			if (s[i] == l[j])
 			{
 				k = j;
-
-				s[i] = dest[k];
+				s[i] = e[k];
 			}
 			j++;
 		}
-
 		i++;
 	}
-
 	return (s);
 }
-
