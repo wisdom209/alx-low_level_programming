5b5654583c0928a87531490fe035f63c0bccc283
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-24 08:59:29 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,28 +1,3 @@
-#include "main.h"
-/**
- * _strrev - reverses a string
- * @s: string param
- *
- * Return: nothing
- */
-void *_strrev(char *s)
-{
-	int i, c = 0;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (i = 0; i < c / 2; i++)
-	{
-		int j = c - i - 1;
-		char temp = *(s + i);
-		*(s + i) = *(s + j);
-		*(s + j) = temp;
-	}
-}
-
 /**
  * _strlen - gets length of a string
  * @s: string param
@@ -41,49 +16,83 @@ int _strlen(char *s)
 }
 
 /**
- * infinite_add - adds two strings
- * @n1: string param
- * @n2: string param
- * @r: buffer to hold result
- * @size_r: max size of buffer
- * Return: sum of strings
+ * _strrev - reverses a string
+ *
+ * @n:string input
+ *
+ * Return:void
+ */
+void _strrev(char *n)
+{
+	int c = 0;
+	int i = 0;
+	char temp;
+
+	while (n[c] != '\0')
+	{
+		c++;
+	}
+
+	for (i = 0; i < c / 2; i++)
+	{
+		int j = c - i - 1;
+
+		temp = *(n + i);
+		*(n + i) = *(n + j);
+		*(n + j) = temp;
+	}
+}
+
+/**
+ *infinite_add - adds two long numbers,stores them.in a buffer
+ *
+ *@n1:one of the two numbers
+ *@n2:2nd no.
+ *@r:buffer to store result
+ *@size_r:buffer size
+ *
+ *Return:pointer to result
+ *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int ln1 = _strlen(n1);
-	int ln2 = _strlen(n2);
+	int carry = 0;
+	int l1 = 0, l2 = 0, digits = 0;
+	int val1 = 0, val2 = 0, sum = 0;
 
-	ln1--;
-	ln2--;
+	l1 = _strlen(n1);
+	l2 = _strlen(n2);
 
-	int sum, carry, val1, val2, digits = 0;
+	l1--;
+	l2--;
 
-	if (ln1 >= size_r || ln2 >= size_r)
+	if (l1 >= size_r || l2 >= size_r)
 		return (0);
 
-	while (ln1 >= 0 || ln2 >= 0 || carry)
+	while (l1 >= 0 || l2 >= 0 || carry)
 	{
-		if (ln1 < 0)
+		if (l1 < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + ln1) - '0';
-		if (ln2 < 0)
+			val1 = *(n1 + l1) - '0';
+
+		if (l2 < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + ln2) - '0';
+			val2 = *(n2 + l2) - '0';
 
 		sum = (val1 + val2 + carry) % 10;
-		carry = (val1 + val2) / 10;
+		carry = (val1 + val2 + carry) / 10;
+
+		if (digits >= (size_r - 1))
+			return (0);
+
 		*(r + digits) = sum + '0';
 
 		digits++;
-		ln1--;
-		ln2--;
+		l1--;
+		l2--;
 	}
-
-	if (digits >= size_r)
-		return (0);
-
 	*(r + digits) = '\0';
 	_strrev(r);
 	return (r);
