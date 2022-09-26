fe37eff1cb9dae4ce1b1586881def890ff6bfb45
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-26 07:33:35 -0700

    my memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - just like memset function
+ * @s: string
+ * @b: one byte character
+ * @n: number of bytes to copy
+ *
+ * Return: replaced string
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		s[i] = b;
+		*(s + i) = b;
 	}
-	return s;
+	return (s);
 }
