62ed2141ab5f81c30becce85cebcb24f88fcc1d6
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-28 15:44:23 -0700

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * findLen - finds string leng
+ * _strlen_recursion - finds string leng
  * @s: string to find length
  *
  * Return: returns nothing
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (1);
 	else
-		return (*s + findLen(s + 1));
+		return (*s + _strlen_recursion(s + 1));
 }
