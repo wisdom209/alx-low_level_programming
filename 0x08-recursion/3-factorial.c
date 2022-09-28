26a87872135b0741e4065bb00a833459fd6dba68
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-28 16:15:50 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,12 @@
  */
 int factorial(int n)
 {
-	if (n == 0)
-		return (1);
-
-	return (n * factorial(n - 1));
+	if (n < 0)
+		return(-1);
+	else
+	{
+		if (n == 0)
+			return (1);
+		return (n * factorial(n - 1));
+	}
 }
