c6934c66af5a2a8e4d068de5f3120fa8582b6ce9
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-30 14:36:30 -0700

    print num of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of args plus file name
+ * @argv: array of args
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int numOfArgs = argc - 1;
+
+	printf("%d\n", numOfArgs);
+
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 int _putchar(char c);
 #endif
