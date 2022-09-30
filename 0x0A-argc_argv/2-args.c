d7c807649c55451910396f1134f26028fe678da4
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-30 15:12:43 -0700

    print args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,16 @@
  *
  * Return: 0 if successful
  */
-int main(int argc, char *argv[] __attribute__((__unused__)))
+int main(int argc __attribute__((__unused__)),
+char *argv[] __attribute__((__unused__)))
 {
 	int numOfArgs = argc - 1;
 	int i = 0;
+
+	if (numOfArgs <= 1)
+	{
+		return (0);
+	}
 
 	for (i = 0; i < numOfArgs; i++)
 	{
