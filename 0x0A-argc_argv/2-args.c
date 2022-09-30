f42b0fdfac048eca483dcfa64cafe6cf07783d44
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-09-30 14:54:34 -0700

    print args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of args plus file name
+ * @argv: array of args
+ *
+ * Return: 0 if successful
+ */
+int main(int argc __attribute__((__unused__)),
+		char *argv[] __attribute__((__unused__)))
+{
+	int numOfArgs = argc - 1;
+	int i = 0;
+
+	for (i = 0; i < numOfArgs; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
