49e50ecc5578fc90cf1d86b2e7f9555a5460f588
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-03 08:31:24 -0700

    2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicate
+ */
+char *_strdup(char *str)
+{
+	char *newStr;
+	int strsize = 0, i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[strsize] != '\0')
+	{
+		strsize++;
+	}
+
+	newStr = (char *)malloc(sizeof(char) * strsize);
+
+	if (newStr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strsize; i++)
+	{
+		newStr[i] = str[i];
+	}
+
+	newStr[strsize] = '\0';
+
+	return (newStr);
+}
