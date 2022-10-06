dcd8c00031608c50c08e101571d708a08731f352
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-06 14:15:14 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = malloc(size * nmemb);
+
+	if (!ptr)
+		return (NULL);
 	int filler = 0;
 	int total = size * nmemb;
 	int i = 0;
 	char *p = ptr;
-
-	if (!ptr)
-		return (NULL);
 
 	for (i = 0; i < total; i++)
 	{
