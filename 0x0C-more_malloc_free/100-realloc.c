413b0c7f1b8683099cef5b58bbdc5c84d4260a90
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-06 18:39:42 -0700

    update

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates space
+ * @ptr: old block
+ * @old_size: old size
+ * @new_size: new size
+ *
+ * Return: pointer to new block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i = 0;
+	char *newPtr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		newPtr = malloc(new_size);
+		if (!newPtr)
+			return (NULL);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size > old_size)
+	{
+		newPtr = malloc(new_size);
+
+		if (!newPtr)
+			return (NULL);
+
+		for (i = 0; i < old_size; i++)
+		{
+			/* newPtr[i] = *((char *)ptr + 1); */
+			newPtr[i] = *((char *)ptr + i);
+		}
+
+		free(ptr);
+	}
+	return (newPtr);
+}
