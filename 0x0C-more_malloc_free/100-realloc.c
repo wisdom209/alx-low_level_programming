e80d5ccaf75552b80f27d942e3e8a826ef4b1d25
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-08 18:07:19 -0700

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (new_size > old_size && (ptr != NULL)
+	if (new_size > old_size && (ptr != NULL))
 	{
 		newPtr = malloc(new_size);
 
