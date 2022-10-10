116be87722a5ad71219f615eec1ee85c12afcc6c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 16:13:06 -0700

    init struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - initializes dog struct
+ * @d: pointer to dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
