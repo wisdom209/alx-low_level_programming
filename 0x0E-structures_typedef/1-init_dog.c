36995474848a9b5c96b8788b395006b2ec9ef525
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 16:15:34 -0700

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->owner = owner;
-	d->age = age;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
 }
