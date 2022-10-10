c58b9ed270305d3fb37dfbfe2a3c95c2e125a821
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 16:21:13 -0700

    update

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - prints dog member values
+ * @d: dog instance
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (!d->age)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s", d->owner);
+	}
+}
