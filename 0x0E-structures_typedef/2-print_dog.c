3f3d01665bcdfe9102865f17ea4011369a683869
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 16:40:13 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!d->age)
+		if (d->age == 0)
 			printf("Age: %f\n", nilDouble);
 		else
 			printf("Age: %f\n", d->age);
