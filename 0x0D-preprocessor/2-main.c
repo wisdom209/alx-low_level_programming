505e696eca10cc4e527b939a3e6eea51e8e9e385
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 18:56:01 -0700

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @void: takes no params
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("The current file is %s\n", __FILE__);
+	return (0);
+}
