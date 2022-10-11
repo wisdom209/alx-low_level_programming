313d3e3d8a6e3be0f4e50c1a2692c1652569983b
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 17:50:19 -0700

    update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_dog - free space occupied by dog
+ * @d: dog instance
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
