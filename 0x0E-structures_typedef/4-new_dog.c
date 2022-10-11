a728ed01b1a5bb28b95841c92097d813555dacd4
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-10 17:06:31 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,35 +13,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy;
 
-	if (name == NULL && owner == NULL)
-		return (NULL);
-
-	puppy = (dog_t *)malloc(sizeof(dog_t));
-	if (!puppy)
+	if (name != NULL && owner != NULL)
 	{
-		return (NULL);
+		puppy = (dog_t *)malloc(sizeof(dog_t));
+		if (!puppy)
+		{
+			return (NULL);
+		}
+
+		puppy->name = malloc(sizeof(name));
+		if (!puppy->name)
+		{
+			free(puppy);
+			return (NULL);
+		}
+
+		strcpy(puppy->name, name);
+
+		puppy->owner = malloc(sizeof(owner));
+		if (!puppy->owner)
+		{
+			free(puppy->name);
+			free(puppy);
+			return (NULL);
+		}
+
+		strcpy(puppy->owner, owner);
+
+		puppy->age = age;
 	}
-
-	puppy->name = malloc(sizeof(name));
-	if (!puppy->name)
-	{
-		free(puppy);
-		return (NULL);
-	}
-
-	strcpy(puppy->name, name);
-
-	puppy->owner = malloc(sizeof(owner));
-	if (!puppy->owner)
-	{
-		free(puppy->name);
-		free(puppy);
-		return (NULL);
-	}
-
-	strcpy(puppy->owner, owner);
-
-	puppy->age = age;
 
 	return (puppy);
 }
