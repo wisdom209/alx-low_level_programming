34a6c1b50ceec190716353940b26cc149e15ac23
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-12 14:29:55 -0700

    update

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include "string.h"
+/* #include "3-get_op_func.c" */
+
+/**
+ * main - command line math
+ * @argc: num of args + file
+ * @argv: array of args
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	char *op = NULL;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error count\n");
+		exit(98);
+	}
+
+	op = argv[2];
+
+	if ((strcmp(op, "/") == 0 && num2 == 0) ||
+		(strcmp(op, "%") == 0 && num2 == 0))
+	{
+		printf("Error div\n");
+		return (100);
+	}
+
+	if (strcmp(op, "+") == 0 || strcmp(op, "*") == 0 ||
+		strcmp(op, "/") == 0 || strcmp(op, "-") == 0 ||
+		strcmp(op, "%") == 0)
+	{
+		int a = (get_op_func(op))(num1, num2);
+
+		printf("%d\n", a);
+		return (0);
+	}
+	printf("Error\n");
+	return (99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - does specified math
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - does specified math
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - does specified math
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - does specified math
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- does specified math
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
