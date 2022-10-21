817139e192ec8db2473f61aaeba1e22d4217be51
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-20 19:21:54 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * hare - comes before main
+ * @void - no params
+ *
+ * Return: void
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
