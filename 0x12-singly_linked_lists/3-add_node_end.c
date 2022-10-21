547b49fae02b3da186980e26ef848eeb03d955c8
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-20 18:33:49 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,11 +14,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp;
 	list_t *newnode;
 
+	if (s == NULL)
+		return (NULL);
+
 	while (s[size] != '\0')
 		size++;
-
-	if (s == NULL)
-		s = "(nil)";
 
 	newnode = malloc(sizeof(list_t));
 
