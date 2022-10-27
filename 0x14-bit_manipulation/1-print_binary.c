4d6c21ef68c31d03035d497ea2191b6cd5820fc9
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-27 11:42:03 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary conversion of n
+ * @n: number to print
+ *
+ * Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	int i = 0, j = 0, flag_first_zero = 0;
+
+	for (i = 32; i >= 0; i--)
+	{
+		j = n >> i;
+
+		if (j & 1)
+			flag_first_zero = 1;
+
+		if (flag_first_zero)
+		{
+			if (j & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+	if (flag_first_zero == 0)
+		_putchar('0');
+}
+
