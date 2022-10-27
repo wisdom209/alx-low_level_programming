a9d8f4dbfeb409160eb9b7699800028ca7f1c456
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-27 12:25:00 -0700

    update

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - gets bit at an indes
+ * @n: num
+ * @index: index
+ *
+ * Return: int
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n & (1 << index);
+
+	if (n > 0)
+		return (1);
+	else
+		return (0);
+}
+
