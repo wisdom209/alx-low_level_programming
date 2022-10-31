2448d1b36e672997aa98f22caaf4f7efd89bfb4e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-31 13:24:24 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,6 +28,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	buf[readBytes] = '\0';
+
 	writtenBytes = write(STDOUT_FILENO, buf, readBytes);
 
 	if (writtenBytes < 0)
