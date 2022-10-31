23bc499ce75bd74859b92ef53dc7e288628a885d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-31 13:17:10 -0700

    update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file
+ * @filename: filename
+ * @letters: no of chars written
+ *
+ * Return: int
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf = malloc(sizeof(char) * letters);
+	int fd, readBytes, writtenBytes;
+
+	fd = open(filename, O_RDONLY);
+
+	if (!buf || filename == NULL || fd < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	readBytes = read(fd, buf, letters);
+
+	if (readBytes < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	writtenBytes = write(STDOUT_FILENO, buf, readBytes);
+
+	if (writtenBytes < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	return (writtenBytes);
+}
+
