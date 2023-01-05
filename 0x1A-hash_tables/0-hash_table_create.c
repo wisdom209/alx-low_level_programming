432aa99ea6cf16aba89191db1ee4cc6508275431
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-05 23:11:39 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = calloc(ht->size, sizeof(hash_node_t));
+	ht->array = calloc(size, sizeof(hash_node_t *));
 
 	if (ht->array == NULL)
 	{
